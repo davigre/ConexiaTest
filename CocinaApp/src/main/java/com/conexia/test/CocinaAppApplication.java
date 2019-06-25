f10c59ea8f9602614ffff7ccd8cbb6285f4ae3fe
David Mendez <davidgmendezn@gmail.com>
2019-06-25 16:35:52 -0500

    Cambios para generar un war.

--- CocinaApp/src/main/java/com/conexia/test/CocinaAppApplication.java
+++ CocinaApp/src/main/java/com/conexia/test/CocinaAppApplication.java
@@ -2,9 +2,16 @@ package com.conexia.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CocinaAppApplication {
+public class CocinaAppApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CocinaAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CocinaAppApplication.class, args);
